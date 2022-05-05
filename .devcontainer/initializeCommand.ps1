021396ed6f5d22ee8a4b92c1f5846ebc81d88fd8
Alex Parsons <alex.parsons@mysociety.org>
2022-05-05 09:33:42 +0000

    Tidfied up devcontainer startup commands

---
+++ .devcontainer/initializeCommand.ps1
@@ -0,0 +1,7 @@
+$Folder = '"src/data_common/src"'
+"Test to see if folder [$Folder]  exists"
+if (Test-Path -Path $Folder) {
+    echo  "Submodule already exists"
+} else {
+    git submodule update --init
+}
