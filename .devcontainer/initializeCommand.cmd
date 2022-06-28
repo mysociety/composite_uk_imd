81acaf50eac84f8fd76f01eaddd7b9d7b268c3e5
Alex Parsons <alex.parsons@mysociety.org>
2022-06-28 11:16:42 +0100

    Update init to cross platform

---
+++ .devcontainer/initializeCommand.cmd
@@ -0,0 +1,1 @@
+powershell .devcontainer/initializeCommand.ps1
--- .devcontainer/initializeCommand.ps1
+++ .devcontainer/initializeCommand.ps1
@@ -1,5 +1,5 @@
-$Folder = '"src/data_common/src"'
-"Test to see if folder [$Folder]  exists"
+$Folder = 'src/data_common/src'
+"Test to see if folder [$Folder] exists"
 if (Test-Path -Path $Folder) {
     echo  "Submodule already exists"
 } else {
