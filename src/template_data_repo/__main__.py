398ba485b6bae41af5e34070ae5145b2e788012c
Alex Parsons <alex.parsons@mysociety.org>
2022-07-12 14:45:43 +0000

    Seperate CLI for project

--- src/template_data_repo/__main__.py
+++ src/template_data_repo/__main__.py
@@ -1,16 +1,18 @@
-from data_common.management.cli import cli, set_doc_collection
-from data_common.management.render_processing import DocumentCollection
-from pathlib import Path
+import rich_click as click
 
-if Path("render.yaml").exists():
-    doc_collection = DocumentCollection.from_yaml("render.yaml")
-    set_doc_collection(doc_collection)
 
-# in principle using the click syntax you could hook further commands on the 'cli' group here.
+@click.group()
+def cli():
+    pass
 
 
 def main():
     cli()
+
+
+@cli.command()
+def example():
+    print("This is an example function")
 
 
 if __name__ == "__main__":
