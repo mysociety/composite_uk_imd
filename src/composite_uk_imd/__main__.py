8af3c7c2311a6a1d5657a45bd8314150d8fddb09
Alex Parsons <alex@alexparsons.co.uk>
2022-09-06 08:59:55 +0000

    Add local authority level data

--- src/composite_uk_imd/__main__.py
+++ src/composite_uk_imd/__main__.py
@@ -1,11 +1,13 @@
 import rich_click as click
 from .create_missing_overall_scores import fill_in_scores
 from .merge_indexes import produce_indexes_and_analysis
-
+from data_common.management.run_notebook import run_notebook
+from pathlib import Path
 
 def update_data_and_build():
     fill_in_scores()
     produce_indexes_and_analysis()
+    run_notebook(Path("notebooks", "generate_la_imd.ipynb"))
 
 
 @click.group()
