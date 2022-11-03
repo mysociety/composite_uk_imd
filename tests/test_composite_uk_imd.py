7353482e54a08df2d0312bd7c566dd1d40499c13
Alex Parsons <alex@alexparsons.co.uk>
2022-11-03 13:39:26 +0000

    Added explicit quin and pop columns to la data

--- tests/test_composite_uk_imd.py
+++ tests/test_composite_uk_imd.py
@@ -2,6 +2,25 @@ import composite_uk_imd
 
 import pytest
 
+from pathlib import Path
+import pandas as pd
 
-def test_true_is_true():
-    assert True is True
+packages_path = Path(__file__).parent.parent / "data" / "packages"
+
+
+def test_all_las():
+    """
+    Test we have the right number of LAs
+    """
+    df = pd.read_csv(packages_path / "uk_index" / "la_labels.csv")
+    no_dupes = df["local-authority-code"].drop_duplicates()
+    assert len(no_dupes) == 409
+
+
+def test_all_lsoas():
+    """
+    Test we have the right number of lsoa
+    """
+    df = pd.read_csv(packages_path / "uk_index" / "UK_IMD_E.csv")
+    no_dupes = df["lsoa"].drop_duplicates()
+    assert len(no_dupes) == 42619
